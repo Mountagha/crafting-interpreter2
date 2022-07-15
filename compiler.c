15cd48712ea8033e01769e76744480ef2cbbcad7
Mountagha <muntaghaba@gmail.com>
2022-07-14 23:49:30 -0400

    on this for class

--- compiler.c
+++ compiler.c
@@ -419,8 +419,13 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
     Local* local = &current->locals[current->localCount++];
     local->depth = 0;
     local->isCaptured = false;
-    local->name.start = "";
-    local->name.length = 0;
+    if (type != TYPE_FUNCTION) {
+        local->name.start = "this";
+        local->name.length = 4;
+    } else {
+        local->name.start = "";
+        local->name.length = 0;
+    }
 }
 
 static void number(bool canAssign) {
@@ -478,6 +483,10 @@ static void variable(bool canAssign) {
     namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+    variable(false);
+}
+
 static void unary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
 
@@ -527,7 +536,7 @@ ParseRule rules[] = {
     [TOKEN_PRINT]           =   {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN]          =   {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER]           =   {NULL, NULL, PREC_NONE},
-    [TOKEN_THIS]            =   {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS]            =   {this_, NULL, PREC_NONE},
     [TOKEN_TRUE]            =   {literal, NULL, PREC_NONE},
     [TOKEN_VAR]             =   {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE]           =   {NULL, NULL, PREC_NONE},
