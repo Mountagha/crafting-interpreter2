30b6042b24e3514dcfce678bac0fbdb37226666e
Mountagha <muntaghaba@gmail.com>
2023-07-15 23:05:42 -0400

    fixed the memory leak and enforced valgrind check during CI pipeline

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ do
     if [[ $f == *.lox ]]
     then
         echo -e "==== Running test for $f with valgrind memChecks ====\n"
-        valgrind --leak-check-full --show-leak-kinds=all --error-exit-code=-1 $1 $f 
+        valgrind --leak-check=full --show-leak-kinds=all --error-exit-code=-1 $1 $f 
         echo -e "\n";
     fi
 done
