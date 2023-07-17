d288c0ce1677c9af048e5c3e102072158229d03a
Mountagha <muntaghaba@gmail.com>
2023-07-16 23:25:44 -0400

    on enforcing valgrind

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,11 @@ do
     if [[ $f == *.lox ]]
     then
         echo -e "==== Running test for $f with valgrind memChecks ====\n"
-        valgrind --leak-check=full --show-leak-kinds=all --error-exit-code=-1 $1 $f 
+        valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=-1 $1 $f 
+        if [[ $? == -1 ]]
+        then
+            exit 1
+        done
         echo -e "\n";
     fi
 done
