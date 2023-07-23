5b2e408ddc93308da17886fb82c7a348b0dc8420
Mountagha <muntaghaba@gmail.com>
2023-07-23 15:59:26 -0400

    adding small string tag

--- value.h
+++ value.h
@@ -55,6 +55,7 @@ typedef enum {
     VAL_BOOL,
     VAL_NIL,
     VAL_NUMBER,
+    VAL_SMALL_STRING,
     VAL_OBJ
 } ValueType;
 
@@ -68,19 +69,22 @@ typedef struct {
     } as;
 } Value;
 
-#define IS_BOOL(value)      ((value).type == VAL_BOOL)
-#define IS_NIL(value)       ((value).type == VAL_NIL)
-#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
-#define IS_OBJ(value)       ((value).type == VAL_OBJ)
+#define IS_BOOL(value)          ((value).type == VAL_BOOL)
+#define IS_NIL(value)           ((value).type == VAL_NIL)
+#define IS_NUMBER(value)        ((value).type == VAL_NUMBER)
+#define IS_SMALL_STRING         ((value).type == VAL_SMALL_STRING)
+#define IS_OBJ(value)           ((value).type == VAL_OBJ)
 
-#define AS_OBJ(value)       ((value).as.obj)
-#define AS_BOOL(value)      ((value).as.boolean)
-#define AS_NUMBER(value)    ((value).as.number)
+#define AS_OBJ(value)           ((value).as.obj)
+#define AS_SMALL_STRING(value)  ((value).as.smallString)
+#define AS_BOOL(value)          ((value).as.boolean)
+#define AS_NUMBER(value)        ((value).as.number)
 
-#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL             ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object)     ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+#define BOOL_VAL(value)         ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL                 ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)       ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)         ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+#define OBJ_SMALL_STRING(value) ((Value){VAL_SMALL_STRING, {memcpy(.smallString, &value, 5)}})
 
 #endif
 
